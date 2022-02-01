16835a90125afc5d706a4edf7fb5cfa045d17994
BhumiRaval12 <bhumir@zignuts.com>
2022-02-01 16:09:52 +0530

    first commit

---
+++ CRUD_Op/index.js
@@ -0,0 +1,38 @@
+let express = require('express');
+let dotenv = require('dotenv');
+let morgan = require('morgan');
+let bodyparser = require("body-parser");
+let path = require('path');
+
+
+let app = express();
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+// log requests
+app.use(morgan('tiny'));
+
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({
+     extended: true
+}))
+
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+
+
+app.get('/', (req, res) => {
+     res.render('crud');
+});
+
+
+
+app.listen(PORT, () => {console.log(`Server is running on http://localhost:${PORT}`)});
